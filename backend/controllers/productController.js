636f2c794e1d051101e7cf68f650b203a6df63bd
Prathamesh Ghormare <prathameshghormare@gmail.com>
2025-03-03 21:36:29 +0530

    add admin auth middleware and admin login functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -63,12 +63,25 @@ const listProduct = async (req, res) => {
 
 //function to remove product
 const removeProduct = async (req, res) => {
-
+    try{
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "product deleted successfully"})
+    }catch(error){
+        console.log(error)
+        res.json({success: true, message: "Internal server error"})
+    }
 }
 
 //function for a single product info
 const singleProduct = async (req, res) => {
-
+    try{
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+    }catch(error){
+        console.log(error)
+        res.json({success: true, message: "Internal server error"})
+    }
 }
 
 export {addProduct, listProduct, removeProduct, singleProduct}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/productController.js'
 import upload from '../middleware/multer.js'
+import adminAuth from '../middleware/adminAuth.js'
 
 const producterRouter = express.Router()
 
-producterRouter.post('/add', upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), addProduct)
-producterRouter.post('/delete', removeProduct)
+producterRouter.post('/add', adminAuth, upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), addProduct)
+producterRouter.post('/delete', adminAuth, removeProduct)
 producterRouter.post('/single', singleProduct)
 producterRouter.get('/list', listProduct)
 
