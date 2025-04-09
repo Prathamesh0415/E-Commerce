32a1cf6fc36b971872e5467c8d3c5942b87c1520
Prathamesh Ghormare <prathameshghormare@gmail.com>
2025-04-09 23:42:00 +0530

    implement stripe payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
+import Stripe from "stripe"
+
+//global variables
+const currency = 'inr'
+const deliveryCharges = 10
+
+//gateway stripe
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //COD
 export const placeOrder = async (req, res) => {
@@ -29,7 +37,70 @@ export const placeOrder = async (req, res) => {
 
 //Stripe
 export const placeOrderStripe = async (req, res) => {
+    try{
+        const { userId, items, amount, address } = req.body
+        const { origin } = req.headers        
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
 
+        const newOrder = await orderModel(orderData)
+        await newOrder.save()
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_Data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity 
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_Data: {
+                    name: 'Delivery Charges'
+                },
+                unit_amount: deliveryCharges*100
+            },
+            quantity: 1 
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment'
+        })
+
+        res.json({success: true, session_url: session.url})
+
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: "Internal Server Error"})
+    }
+}
+
+//Verify Payment stripe
+export const verifyStripe = async (req, res) => {
+    try{
+        const { orderId, success, userId } = req.body
+        await orderModel.findByIdAndUpdate(orderId, {payment: true})
+        await userModel.findByIdAndUpdate(userId, {cartData:{}})
+        res.json({success: true})
+    }catch(error){
+        console.log("Error in order controller", error)
+        res.json({success: false})
+    }
 }
 
 //RazorPay
